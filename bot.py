70bf6bb4e4af08f0e14d0082f4ab4c6817d7d78b
Sean Yager <48184873+MisuseofMana@users.noreply.github.com>
2026-04-07 20:28:06 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import json
 import os
 
+bot.run(os.getenv("DISCORD_TOKEN"))
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
